45828d70f8b12307832088d58d3b691c0bf2ebb0
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2018-05-03 08:04:33 +0900

    add pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,11 +23,31 @@ module.exports = {
   modules: [
     '@nuxtjs/vuetify',
     '@nuxtjs/markdownit',
-    '@nuxtjs/google-analytics'
+    '@nuxtjs/google-analytics',
+    '@nuxtjs/pwa'
   ],
   loading: { color: '#03A9F4' },
   'google-analytics': {
     id: 'UA-81299115-1'
+  },
+  manifest: {
+    name: 'Hagi',
+    short_name:
+      'Hagi',
+    title:
+      'Hagi',
+    'og:title':
+      'Hagi - portfolio website',
+    description:
+      'portfolio website',
+    'og:description':
+      'portfolio website',
+    lang:
+      'ja',
+    theme_color:
+      '#03A9F4',
+    background_color:
+      '#ffffff'
   },
   /*
   ** Build configuration
