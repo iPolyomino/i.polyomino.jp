d6ff65a5d0cbcd1429e0532447851d1b17b44855
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2018-09-20 17:03:58 +0900

    Revert "make blog index page"
    This reverts commit 72b2795b16fe59728f502f227c4b58d9e93779f7.

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -5,12 +5,9 @@
     <navigation-component/>
     <v-content class="main-contents">
       <v-flex xs12 sm8 offset-sm2>
-        <v-card color="light-blue lighten-5" class="mx-2 my-2" v-for="article in fileMap" :key="article.base">
+        <v-card color="light-blue lighten-5" class="mx-2 my-2">
           <v-container fluid grid-list-lg>
-            <h1>{{article.title}}</h1>
-            <p class="text-xs-right">{{article.date}}</p>
-            <p>{{article.preview}}</p>
-            <v-btn flat large color="light-blue" :href="getUrl(article.base)">記事ページヘ</v-btn>
+            <div>test</div>
           </v-container>
         </v-card>
         <footer-component/>
@@ -25,25 +22,12 @@ import BackgroundComponent from "~/components/Background.vue";
 import NavigationComponent from "~/components/Navigation.vue";
 import FooterComponent from "~/components/Footer.vue";
 import favoriteLanguage from "~/static/favoriteLanguage.md";
-import summary from "~/posts/json/summary.json";
 
 export default {
   components: {
     BackgroundComponent,
     NavigationComponent,
     FooterComponent
-  },
-  asyncData() {
-    return summary;
-  },
-  methods: {
-    getUrl(jsonFileName) {
-      const url = jsonFileName.match(/\d{4}-\d{2}-\d{2}-\d{4}/g);
-      if (url == null || url.length !== 1) {
-        return null;
-      }
-      return `/blog/${url[0]}`;
-    }
   }
 };
 </script>
