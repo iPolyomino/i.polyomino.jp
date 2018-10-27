4d6779f27e0ea505201a3d122ea83b354b9ab1dc
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2018-10-27 14:27:22 +0900

    fix code format

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,14 +1,14 @@
- <template>
-  <v-toolbar app fixed clipped-left dark color="grey">
-    <v-toolbar-title>
-      <nuxt-link class="white--text headline" to="/">polyomino.jp</nuxt-link>
-    </v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-toolbar-side-icon class="hidden-md-and-up" @click.stop="toggleDrawer"></v-toolbar-side-icon>
-    <v-toolbar-items class="hidden-sm-and-down" v-for="link in links" :key="link.title">
-      <v-btn class="headline" flat :to="link.link">{{link.title}}</v-btn>
-    </v-toolbar-items>
-  </v-toolbar>
+<template>
+<v-toolbar app fixed clipped-left dark color="grey">
+  <v-toolbar-title>
+    <nuxt-link class="white--text headline" to="/">polyomino.jp</nuxt-link>
+  </v-toolbar-title>
+  <v-spacer></v-spacer>
+  <v-toolbar-side-icon class="hidden-md-and-up" @click.stop="toggleDrawer"></v-toolbar-side-icon>
+  <v-toolbar-items class="hidden-sm-and-down" v-for="link in links" :key="link.title">
+    <v-btn class="headline" flat :to="link.link">{{link.title}}</v-btn>
+  </v-toolbar-items>
+</v-toolbar>
 </template>
 
 <script>
