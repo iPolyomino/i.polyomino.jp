d5bc42b63acac3f6cab43e09e6bd33e659d1b7c3
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-07-29 00:57:23 +0900

    fix lint: .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,19 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     "plugin:vue/essential",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
   // required to lint *.vue files
   plugins: ["vue"],
   // add your custom rules here
-  rules: {}
+  rules: {},
 };
