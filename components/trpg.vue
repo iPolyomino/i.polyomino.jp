fdebfc981356daab67950c6ad47b47adbdf28e36
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-03-17 16:37:01 +0900

    support

---
+++ components/trpg.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card color="light-blue lighten-5" class="mx-2 my-2">
+    <v-container>
+      <h1>TRPG DICE プライバシーポリシー</h1>
+      <p>本アプリケーションはユーザ情報を収集しません。</p>
+      <p></p>
+      <hr />
+      <h1>TRPG DICE Privacy Policy</h1>
+      <p>This application does not collect user information.</p>
+    </v-container>
+  </v-card>
+</template>
---
+++ pages/support/trpg-dice.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-main>
+    <v-flex xs12 sm8 offset-sm2>
+      <trpg-component />
+      <footer-component />
+    </v-flex>
+  </v-main>
+</template>
+
+<script>
+import TrpgComponent from "~/components/trpg.vue";
+import FooterComponent from "~/components/Footer.vue";
+
+export default {
+  components: {
+    TrpgComponent,
+    FooterComponent,
+  },
+};
+</script>
